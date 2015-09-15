8236d93c827dc77049f9de06ef6c46183ecd9150
Tiago Royer <royertiago@gmail.com>
2015-09-15 10:31:30 -0300

    Add xorshift.cpp.

---
+++ xorshift.cpp
@@ -0,0 +1,23 @@
+/* /dev/urandom-like interface to the xorshift random number generator.
+ * (The output format is suited for the dieharder test suite.)
+ */
+#include <iostream>
+#include <gmpxx.h>
+#include "random/xorshift.hpp"
+
+union undefined_behavior {
+    unsigned num;
+    char str[4];
+};
+
+int main() {
+    rng::xorshift rng;
+
+    // This kludge invokes undefined behavior, but well... it works and its short.
+    undefined_behavior ub;
+
+    while( true ) {
+        ub.num = rng();
+        std::cout.write( ub.str, 4 );
+    }
+}
