4b47bbf57b70eae91e9c7846be800850e2ed1cdd
Tiago Royer <royertiago@gmail.com>
2015-09-27 10:17:17 -0300

    primitive_root.cpp: Extract algorithm to math/primitive_root.hpp.

---
+++ math/primitive_root.hpp
@@ -0,0 +1,45 @@
+#ifndef MATH_PRIMITIVE_ROOT_HPP
+#define MATH_PRIMITIVE_ROOT_HPP
+
+#include "math/algo.hpp"
+#include "math/factor.hpp"
+
+namespace math {
+    /* Returns the smallest primitive root modulo p,
+     * assuming p is prime,
+     * or 0 if no primitive root could be found.
+     */
+    template< typename T >
+    T primitive_root_modulo_p( T p ) {
+
+        T phi = p - 1;
+        std::vector<T> exponents;
+        for( auto pair : math::factor::trial_division( phi ) )
+            exponents.push_back( phi / pair.first );
+        /* It can be shown that a number m is a primitive root modulo p
+         * if and only if, for every factor f of p-1, m^((p-1)/f) != 1 (mod p).
+         * Thus, we need to raise every candidate to (p-1)/f
+         * for every factor f of p-1.
+         * The vector exponents just precompute this list.
+         */
+
+        T power;
+        for( T candidate(2); candidate < p; candidate++ ) {
+            for( const auto & exponent : exponents )
+                if( math::pow_mod( candidate, exponent, p ) == 1 )
+                    // The candidate failed the test.
+                    goto next_candidate;
+
+            // Found a primitive root!
+            return candidate;
+
+        next_candidate:; // This label functions as a 'continue' command.
+        }
+
+        // No primitive roots found.
+        return T(0);
+    }
+
+} // namespace math
+
+#endif // MATH_PRIMITIVE_ROOT_HPP
--- primitive_root.cpp
+++ primitive_root.cpp
@@ -4,8 +4,7 @@
 
 #include <iostream>
 #include <gmpxx.h>
-#include "math/factor.hpp"
-#include "math/algo.hpp"
+#include "math/primitive_root.hpp"
 
 int main( int argc, char ** argv ) {
     mpz_class prime;
@@ -14,30 +13,10 @@ int main( int argc, char ** argv ) {
         return 1;
     }
 
-    mpz_class phi = prime - 1;
-    std::vector< mpz_class > exponents;
-    for( auto pair : math::factor::trial_division( phi ) )
-        exponents.push_back( phi / pair.first );
-
-    /* It can be shown that a number m is a primitive root modulo p
-     * if and only if, for every factor f of p-1, m^((p-1)/f) != 1 (mod p).
-     * Thus, we need to raise every candidate to (p-1)/f
-     * for every factor f of p-1.
-     * The vector exponents just precompute this list.
-     */
-
-    mpz_class power;
-    for( mpz_class candidate = 2; candidate < prime; candidate++ ) {
-        for( const auto & exponent : exponents )
-            if( math::pow_mod( candidate, exponent, prime ) == 1 )
-                // The candidate failed the test.
-                goto next_candidate;
-
-        // Found a primitive root!
-        std::cout << candidate << " is a primitive root modulo " << prime << std::endl;
+    mpz_class root = math::primitive_root_modulo_p( prime );
+    if( root != 0 ) {
+        std::cout << root << " is a primitive root modulo " << prime << std::endl;
         return 0;
-
-next_candidate:;
     }
 
     std::cout << "No primitive roots found for " << prime << std::endl;
