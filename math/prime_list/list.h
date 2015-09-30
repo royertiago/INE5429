a0e7b503a4d2f592ba65ad292e176b8b9d24b60f
Tiago Royer <royertiago@gmail.com>
2015-09-30 13:31:56 -0300

    Add math/prime_list/list.{cpp,h}
    These files uses the functions in math/prime_list/meta.hpp
    to generate a list with the first 2^13 prime numbers.
    However, compilation takes forever:
    `clang -ftemplate-depth=2000000` takes two minutes and half
    and `gcc -ftemplate-depth=2000000` takes almost 13 minutes.
    
    In a future patch, the template metaprograms will be replaced with
    an actual program that generate the prime list.

---
+++ math/prime_list/list.h
@@ -0,0 +1,28 @@
+#ifndef MATH_PRIME_LIST_LIST_H
+#define MATH_PRIME_LIST_LIST_H
+
+/* List of the first primes,
+ * generated through metaprogramming.
+ *
+ * The prime list is in another translation unit,
+ * to avoid the template instantiation cost.
+ */
+
+namespace math {
+namespace prime_list {
+    // Size of the list.
+    constexpr int size = 1 << 13;
+
+    /* The actual list.
+     * For example,
+     * p[0] == 2,
+     * p[1] == 3, etc.
+     */
+    extern const int * const p;
+
+    // Last prime in the list.
+    extern const int last;
+
+}} // namespace math::prime_list
+
+#endif // MATH_PRIME_LIST_LIST_H
