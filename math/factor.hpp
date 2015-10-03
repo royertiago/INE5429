a5a5f9a561867b72f5dfbed11a68d00c1c400f94
Tiago Royer <royertiago@gmail.com>
2015-10-02 23:00:07 -0300

    math/factor.hpp: Implement Pollard's rho algorithm.

--- math/factor.hpp
+++ math/factor.hpp
@@ -5,7 +5,10 @@
  */
 #include <vector>
 #include <utility>
+#include "math/algo.hpp"
 #include "math/prime_list/list.h"
+#include "prime/fermat.hpp"
+#include "random/xorshift.hpp"
 
 namespace math { namespace factor {
     /* Every function of this namespace returns the list of factors
@@ -61,6 +64,82 @@ namespace math { namespace factor {
         // Finally, add the number itself
         if( n != T(1) )
             factors.push_back( {n, 1} );
+
+        return factors;
+    }
+
+    /* Default function used by Pollard's Rho algorithm.
+     */
+    template< typename T >
+    T pollard_rho_default_function( T n ) {
+        return n * n + 1;
+    }
+
+    /* Pollard's Rho algorithm,
+     * with Brent's cycle detection algorithm.
+     */
+    template< typename T, typename F = T(T) >
+    factor_list<T> pollard_rho( T n, F f = pollard_rho_default_function<T> ) {
+        factor_list<T> factors;
+        rng::xorshift xorshift; // To detect primality
+
+        if( fermat_probably_prime( n, xorshift, 30 ) ) {
+            factors.push_back( {n, 1} );
+            return factors;
+        }
+
+        T i(1); // Current iteration index.
+        T l_i(0); // Value l(i) - 1, the index against which we are comparing to.
+
+        T x_l_i( xorshift() % n ); // X_{l(i) - 1}, the value against which
+        // we will compare to.
+
+        T x_i = f(x_l_i) % n; // X_i; Current value of iteration
+
+        /* We have initialized x_l_i before x_i to guarantee that
+         *  x_i = x_1 = f(x_0) = f(x_l_i).
+         */
+
+        T d = gcd( T(n + x_i - x_l_i), n );
+        /* d is the candidate to a divisor of n.
+         * We will iterate until d == n,
+         * in which the algorithm have failed.
+         */
+
+        while( d != n ) {
+            if( d != T(1) ) {
+                /* Found nontrivial divisor of n!
+                 * Add it to the list and keep factoring.
+                 */
+                factors.push_back( {d, 1} );
+                n /= d;
+                x_i %= n;
+                x_l_i %= n;
+
+                if( fermat_probably_prime( n, xorshift, 30 ) ) {
+                    // No more factoring to do.
+                    factors.push_back( {n, 1} );
+                    return factors;
+                }
+            }
+
+            if( 2*l_i + 1 == i ) {
+                /* Must reset both l_i and x_l_i. */
+                l_i = 2*l_i + 1;
+                x_l_i = x_i;
+            }
+
+            ++i;
+            x_i = f(x_i) % n;
+            d = gcd( T(n + x_i - x_l_i), n );
+        }
+
+        if( d == n ) {
+            /* We failed.
+             * TODO: fail more elegantly.
+             */
+            factors.push_back( {n, 1} );
+        }
 
         return factors;
     }
