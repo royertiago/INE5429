437e338bf897b88cd1fbecd0c952f7aa332fe7f4
Tiago Royer <royertiago@gmail.com>
2015-10-13 12:44:13 -0300

    math/algo.hpp: Add Extended Euclid paraphernalia
    This code was copied from https://github.com/royertiago/INE5451.

--- math/algo.hpp
+++ math/algo.hpp
@@ -33,6 +33,44 @@ namespace math {
         }
         return a;
     }
+
+    /* Data for the Extended Euclid's algorithm.
+     */
+    template< typename T >
+    struct euclid_data {
+        T x;
+        T y;
+        T gcd; // The greatest common divisor
+    };
+
+    template< typename T >
+    euclid_data<T> extended_euclid( T a, T b ) {
+        T xx = T(1), xy = T(0), yx = T(0), yy = T(1);
+        while( b != 0 ) {
+            T nxy = xx - xy*(a/b);
+            xx = xy;
+            xy = nxy;
+
+            T nyy = yx - yy*(a/b);
+            yx = yy;
+            yy = nyy;
+
+            T nb = a % b;
+            a = b;
+            b = nb;
+        }
+        return {xx, yx, a};
+    }
+
+    /* Computes the inverse of a modulo n.
+     * This algorithm assumes gcd(a, n) == 1.
+     */
+    template< typename T >
+    T modular_inverse( T a, T n ) {
+        auto d = extended_euclid( a, n );
+        return (d.x + n) % n;
+    }
+
 } // namespace math
 
 #endif // MATH_ALGO_HPP
