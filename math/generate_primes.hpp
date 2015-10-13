5b1f4cf192f7d1d8fc67ab29be373f0e71514c3f
Tiago Royer <royertiago@gmail.com>
2015-10-13 12:40:15 -0300

    Add math/generate_primes.hpp.

---
+++ math/generate_primes.hpp
@@ -0,0 +1,20 @@
+#ifndef MATH_GENERATE_PRIMES_HPP
+#define MATH_GENERATE_PRIMES_HPP
+
+#include "random/gmp_adapter.hpp"
+
+namespace math {
+
+    template< typename RNG >
+    mpz_class generate_prime_number( RNG & rng, std::uint32_t bits, int trials ) {
+        mpz_class number;
+        do {
+            number = rng::gmp_generate( rng, bits );
+        } while( !math::primality::fermat( number, rng, trials ) );
+
+        return number;
+    }
+
+}
+
+#endif // MATH_GENERATE_PRIMES_HPP
