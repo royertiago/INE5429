d0ff161d5ea16460e0732df482d80a8c0f406d21
Tiago Royer <royertiago@gmail.com>
2016-02-28 18:22:10 -0300

    math/primality.hpp: Optionally return the Fermat witness.

--- math/primality.hpp
+++ math/primality.hpp
@@ -12,17 +12,21 @@ namespace primality {
  * Performs the selected number of trials,
  * using the given random number generator.
  *
+ * If the number is found not to be prime and the 'witness' pointer is not null,
+ * the found Fermat witness to the compositeness of the number will be stored there.
+ * Otherwise, the number is left untouched.
+ *
  * Note that even if the number of trials is enormous,
  * there are some non-prime numbers (the Charmicael numbers)
  * that will pass the Fermat test.
  */
 template< typename RNG >
-bool fermat( mpz_class number, RNG& rng, int trials );
+bool fermat( mpz_class number, RNG& rng, int trials, mpz_class * witness = nullptr );
 
 // Implementation
 
 template< typename RNG >
-bool fermat( mpz_class number, RNG& rng, int trials ) {
+bool fermat( mpz_class number, RNG& rng, int trials, mpz_class * witness ) {
     mpz_class power, witness_candidate;
 
     int bits = mpz_sizeinbase( number.get_mpz_t(), 2 );
@@ -37,10 +41,12 @@ bool fermat( mpz_class number, RNG& rng, int trials ) {
         // Test witness
         power = math::pow_mod( witness_candidate, number_minus_one, number );
 
-        if( power != 1 )
+        if( power != 1 ) {
             /* Failed this trial. By Fermat's little theorem,
              * we are sure this number is not prime. */
+            if( witness ) *witness = witness_candidate;
             return false;
+        }
     }
     // Passed all trials
     return true;
